1692d040bedd8ad5eca6f6429d35b40576fc4fc6
misaewl3 <misaelbrunoroliberato@gmail.com>
2019-07-01 00:42:05 -0300

    Protótipo de interface

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/graficos', title: 'Gráficos',  icon: 'trending_up', class: '' },
+    { path: '/perfil-user', title: 'Perfil de Usuário',  icon: 'person', class: '' },
+    { path: '/mural', title: 'Mural de Perguntas',  icon: 'content_paste', class: '' },
+    { path: '/suasperguntas', title: 'Suas Perguntas',  icon: 'library_books', class: '' },
+    { path: '/chat', title: 'Chat',  icon: 'speaker_notes', class: '' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,57 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { TypographyComponent } from '../../typography/typography.component';
+import { IconsComponent } from '../../icons/icons.component';
+
+export const AdminLayoutRoutes: Routes = [
+    // {
+    //   path: '',
+    //   children: [ {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    // }]}, {
+    // path: '',
+    // children: [ {
+    //   path: 'userprofile',
+    //   component: UserProfileComponent
+    // }]
+    // }, {
+    //   path: '',
+    //   children: [ {
+    //     path: 'icons',
+    //     component: IconsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'notifications',
+    //         component: NotificationsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'maps',
+    //         component: MapsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'typography',
+    //         component: TypographyComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'upgrade',
+    //         component: UpgradeComponent
+    //     }]
+    // }
+    { path: 'graficos',      component: DashboardComponent },
+    { path: 'perfil-user',   component: UserProfileComponent },
+    { path: 'mural',     component: TableListComponent },
+    { path: 'suasperguntas',     component: TypographyComponent },
+    { path: 'chat',          component: IconsComponent },
+];
