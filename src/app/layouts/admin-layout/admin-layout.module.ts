834714ef56a070690e42c20eac574e124fd0a25c
misaewl3 <misaelbrunoroliberato@gmail.com>
2019-07-04 00:35:04 -0300

    Updates nas pastas

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -3,11 +3,11 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+import { TableListComponent } from '../../pages/table-list/table-list.component';
+import { TypographyComponent } from '../../pages/typography/typography.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
 
 import {
   MatButtonModule,
