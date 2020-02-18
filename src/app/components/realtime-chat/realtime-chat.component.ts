00fa113a67eb47d2af1dc361f8b5236791389010
Misael <misaelbrunoroliberato@gmail.com>
2020-02-17 23:44:59 -0300

    updates

--- src/app/components/realtime-chat/realtime-chat.component.ts
+++ src/app/components/realtime-chat/realtime-chat.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../services/user/user.service';
 import { ChatService } from './../../services/chat/chat.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,17 +9,30 @@ import { Component, OnInit } from '@angular/core';
   providers: [ChatService]
 })
 export class RealtimeChatComponent implements OnInit {
-  user: String;
+  currentUser: any;
   room: String;
   messageText: String;
   messageArray: Array<{user: String, message: String}> = [];
 
-  constructor(private chatService: ChatService) { }
+  constructor(
+    private chatService: ChatService,
+    private userService: UserService
+  ) {
+      this.chatService.newUserJoined().subscribe(data => {
+        this.messageArray.push(data);
+      });
+    }
 
   ngOnInit() {
+    this.userService.currentUser().then(res => {
+      this.currentUser = res;
+    });
   }
 
   join() {
+    if (this.currentUser) {
+      this.chatService.joinRoom({user: this.currentUser.nomeVirtual, room: this.room});
+    }
   }
 
   leave() {
--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -9,8 +9,23 @@ import * as io from 'socket.io-client';
 
 export class ChatService {
     private socket;
-    
+
     constructor() {
-        this.socket = io('http://localhost:80');
+        this.socket = io(environment.FREE_URL);
+    }
+
+    joinRoom(data) {
+        this.socket.emit('join', data);
+    }
+
+    newUserJoined() {
+        const observable = new Observable<{user: String, message: String}>(observer => {
+            this.socket.on('new user joined', (data) => {
+                observer.next(data);
+            });
+            return () => { this.socket.disconnect(); }
+        });
+
+        return observable;
     }
 }
